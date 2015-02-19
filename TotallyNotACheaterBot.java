bdf7e845a9bb8050e1fcac7fbe597da75d7f5de6
Jackson Spell <jaspell@davidson.edu>
2015-02-19 18:53:18 -0500

    Added method for lookback in bot.

--- TotallyNotACheaterBot.java
+++ TotallyNotACheaterBot.java
@@ -15,6 +15,15 @@ public class TotallyNotACheaterBot implements RoShamBot {
 	  * @return Action - the bot's move
 	  */
 	public Action getNextMove(Action lastOpponentMove) {
-		
+
+
+		// Let's implement (for starters) an algorithm which remembers the history of the current 
+		// game and can search back k steps (down to a limit search depth) - I've set up a method
+		// below.
+	}
+
+
+	public Action lookBack(Action lastOpponentMove) {
+
 	}
 }
