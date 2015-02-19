87360780d6dddde5bd29340cbbcb8fee07f0558a
Jackson Spell <jaspell@davidson.edu>
2015-02-19 18:39:48 -0500

    Created bot file.

---
+++ TotallyNotACheaterBot.java
@@ -0,0 +1,20 @@
+
+/** Ben and Jackson's Rock Paper Scissors player submission.
+  *
+  * @author Jackson Spell, Ben Wiley
+  */
+
+public class TotallyNotACheaterBot implements RoShamBot {
+
+	/** Returns the bot's next move.
+	  *
+	  *	Parameters:
+	  * @param lastOpponentMove - Action - opponent's previous move
+	  *
+	  * Returns:
+	  * @return Action - the bot's move
+	  */
+	public Action getNextMove(Action lastOpponentMove) {
+		
+	}
+}
