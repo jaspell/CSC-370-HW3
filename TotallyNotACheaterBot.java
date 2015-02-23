644afbbe4a2ab77e69731d9255e749d8f123b55c
Jackson Spell <jaspell@davidson.edu>
2015-02-23 16:13:23 -0500

    Small changes.

--- TotallyNotACheaterBot.java
+++ TotallyNotACheaterBot.java
@@ -1,10 +1,32 @@
 
+import java.util.Random;
+
 /** Ben and Jackson's Rock Paper Scissors player submission.
   *
   * @author Jackson Spell, Ben Wiley
   */
 
+// We should implement an algorithm switcher that, when it gets to the point that playing by 
+// the current algorithm will let the opponent win if they guess correctly, we switch 
+// algorithms to change it up so their algorithm stops being accurate.
+
 public class TotallyNotACheaterBot implements RoShamBot {
+
+	List<Action> myHist;
+	List<Action> opHist;
+
+	/** Constructor for TotallyNotACheaterBot.
+	  *
+	  *	Parameters:
+	  * 	None
+	  *
+	  * Returns:
+	  * 	None
+	  */
+	public TotallyNotACheaterBot() {
+		myHist = new ArrayList<Action>();
+		opHist = new ArrayList<Action>();
+	}
 
 	/** Returns the bot's next move.
 	  *
@@ -16,23 +38,23 @@ public class TotallyNotACheaterBot implements RoShamBot {
 	  */
 	public Action getNextMove(Action lastOpponentMove) {
 
+		opHist.append(lastOpponentMove);
 
-		// We should implement an algorithm switcher that, when it gets to the point that playing by 
-		// the current algorithm will let the opponent win if they guess correctly, we switch 
-		// algorithms to change it up so their algorithm stops being accurate.
+
+		return Action.ROCK;
 	}
 
 	/** Returns move based on opponent's most likely move historically.
 	  *
 	  *	Parameters:
-	  * @param lastOpponentMove - Action - opponent's previous move
 	  * @param steps - int - number of previous steps to look back
 	  * @param maxLookBack - int - max number of historical moves to check
 	  *
 	  * Returns:
 	  * @return Action - move choice
 	  */
-	public Action lookBack(Action lastOpponentMove, int steps, int maxLookBack) {
+	public Action lookBack(int steps, int maxLookBack) {
+
 
 	}
 }
