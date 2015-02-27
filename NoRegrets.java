15d03a20d4e2f7c1714170f2b453a16cd5b5a214
Ben Wiley <samushighwind@gmail.com>
2015-02-26 21:25:08 -0500

    NoRegrets mostly done.

--- NoRegrets.java
+++ NoRegrets.java
@@ -16,6 +16,18 @@ import java.util.*;
 
 public class NoRegrets implements RoShamBot {
 
+	private static final List<Action> MOVES = 
+		Collections.unmodifiableList(Arrays.asList(Action.values()));
+	private static final int TRACEBACK = 30;
+
+	private double[] regretTotal, strategyTotal;
+	private double[] myStrat, actionUtility;//, opStrat // these arrays persist but their values are continually reset.
+	// FOR ALL ARRAYS: 0 = Rock, 1 = Paper, 2 = Scissors
+	//private List<Action> opHist;
+	private Action last;
+	//private int moves;
+
+
 	/** Constructor for NoRegrets.
 	  *
 	  *	Parameters:
@@ -25,7 +37,15 @@ public class NoRegrets implements RoShamBot {
 	  * 	None
 	  */
 	public NoRegrets() {
-		
+		this.regretTotal = new double[3];
+		this.strategyTotal = new double[3];
+		this.myStrat = new double[3];
+		this.actionUtility = new double[3];
+		//this.opStrat = { .3, .3, .3 };
+		//this.myHist = new ArrayList<Action>();
+		//this.opHist = new ArrayList<Action>();
+		//moves = 0;
+		this.last = Action.ROCK;
 	}
 
 	/** Returns the bot's next move.
@@ -37,10 +57,84 @@ public class NoRegrets implements RoShamBot {
 	  * @return Action - the bot's move
 	  */
 	public Action getNextMove(Action lastOpponentMove) {
+		/*this.opHist.add(lastOpponentMove);
+		this.moves++;*/
+
+		int myAction, otherAction;
+		//Rock is 0
+		if(this.last == PAPER) myAction = 1;
+		else if(this.last == SCISSORS) myAction = 2;
+		if(lastOpponentMove == PAPER) otherAction = 1;
+		else if(this.last == SCISSORS) otherAction = 2;
+
+		actionUtility[otherAction] = 0;
+		actionUtility[otherAction == 2 ? 0 : otherAction + 1] = 1;
+		actionUtility[otherAction == 0 ? 2 : otherAction - 1] = -1;
+
+		for(int i = 0; i < 3; i++)
+			regretTotal[i] += actionUtility[i] - actionUtility[myAction];
 
 
-		return myMove;
+		//get new strategy and return next move
+		this.updateStrategy();
+		double d = Math.random();
+		double cumulativeProb = 0;
+		int i;
+		for(i = 0; i < 2 && d >= cumulativeProb; i++)
+			cumulativeProb += this.myStrat[i];
+		if(d < cumulativeProb) i--;
+		if(i == 0) this.last = Action.ROCK;
+		else if(i == 1) this.last = Action.PAPER;
+		else this.last = Action.SCISSORS;
+		return last;
 	}
+
+	/** Updates the bot's mixed strategy.
+	  *
+	  *	Parameters:
+	  * 	None
+	  *
+	  * Returns:
+	  * 	None
+	  */
+	private void updateStrategy() {
+		double normalizingSum = 0;
+		for(int i = 0; i < 3; i++) {
+			if(this.regretTotal[i] > 0)
+				this.myStrat[i] = regretTotal[i];
+			else this.myStrat[i] = 0;
+			normalizingSum += this.myStrat[i];
+		}
+		for(int i = 0; i < 3; i++) {
+			if(normalizingSum > 0)
+				this.myStrat[i] /= normalizingSum;
+			else this.myStrat[i] = 1.0 / 3;
+			this.strategyTotal[i] += this.myStrat[i];
+		}
+	}
+
+	/** Updates the opponent's mixed strategy.
+	  *
+	  *	Parameters:
+	  * 	None
+	  *
+	  * Returns:
+	  * 	None
+	  */
+	/*private void updateOpStrategy() {
+		if(this.moves >= TRACEBACK) {
+			double rock = 0.0;
+			double paper = 0.0;
+			double scissors = 0.0;
+			for(int i = this.moves - 1; i >= this.moves - TRACEBACK; i--) {
+				Action a = opHist.get(i);
+				if(a == ROCK) rock++;
+				else if(a == PAPER) paper++;
+				else scissors++;
+			}
+			this.opStrat = { rock / TRACEBACK, paper / TRACEBACK, scissors / TRACEBACK };
+		}
+	}*/
 
 	/** Returns move that will beat given move.
 	  *
